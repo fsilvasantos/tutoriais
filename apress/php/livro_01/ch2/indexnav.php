b5dab419c9da71579f02759e939c707f0c723878
Felipe Santos <felipesantos@saovicente.sp.gov.br>
2023-11-30 17:26:02 -0300

    Arquivo desnecessário

--- apress/php/livro_01/ch2/indexnav.php
+++
@@ -1,9 +0,0 @@
-<?php
-//complete code for index.php
-include_once "views/navigation.php";
-$pageData = new stdClass();
-$pageData->title = "Thomas Blom Hansen: Portifolio site";
-$pageData->content = $nav;
-require "templates/newerpage.php";
-echo $page;
-?>
--- apress/php/livro_01/ch2/newindex.php
+++
@@ -1,8 +0,0 @@
-<?php
-//complete code for index.php
-$title = "Test title";
-$content = "<h1>Hello World</h1>";
-//indicate the relative path to the file to include
-require "templates/page.php";
-echo $page;
-?>
