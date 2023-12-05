5a5399dd547909b3bee7806ad69ad68466356c93
Felipe Santos <felipesantos@saovicente.sp.gov.br>
2023-12-05 11:08:57 -0300

    IMPROVEMENT - Accessing URL Variables

--- apress/php/livro_01/ch2/index.php
+++ apress/php/livro_01/ch2/index.php
@@ -1,8 +1,16 @@
 <?php
 //complete code for index.php
+include_once "views/navigation.php";
 $pageData = new stdClass();
-$pageData->title = "New, object-oriented test title";
-$pageData->content = "<h1>Hello from an object</h1>";
+$pageData->title = "Thomas Blom Hansen: Portfolio site";
+$pageData->content = $nav;
+//changes begin here
+$navigationIsClicked = isset($_GET['page']);
+if ($navigationIsClicked ) {
+$fileToLoad = $_GET['page'];
+$pageData->content .= "<p>Will soon load $fileToLoad.php</p>";
+}
+//end of changes
 require "templates/page.php";
 echo $page;
 ?>
