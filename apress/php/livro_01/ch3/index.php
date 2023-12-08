7c1e8a3d9fdb835e0e22346045d06cff9cd2ce33
Felipe Santos <felipesantos@saovicente.sp.gov.br>
2023-12-08 16:19:45 -0300

    IMPROVEMENT - Displaying Page Views

--- apress/php/livro_01/ch3/index.php
+++ apress/php/livro_01/ch3/index.php
@@ -1,11 +1,28 @@
 <?php
 //complete code for index.php
-include_once "classes/Page_Data.class.php";
+$nav = "";
+$info = "";
 
+include_once "views/navigation.php";
+
+include_once "classes/Page_Data.class.php";
 $pageData = new Page_Data();
-$pageData->title = "Building and processing HTML forms with PHP";;
-$pageData->content = "<nav>will soon show a navigation...</nav>";
+$pageData->title = "Building and processing HTML forms with PHP";
+$pageData->content = $nav;
 $pageData->content .= "<div>...and a form here</div>";
+
+$navigationIsClicked = isset($_GET['page']);
+if ($navigationIsClicked ) 
+{
+    $fileToLoad = $_GET['page'];
+} 
+else 
+{
+    $fileToLoad = "search";
+}
+
+include_once "views/$fileToLoad.php";
+$pageData->content .= $info;
 
 require "templates/page.php";
 echo $page;
