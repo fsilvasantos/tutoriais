83f68ce8c5fa1e436fad8fc3d11f0473366a53d7
Felipe Santos <felipesantos@saovicente.sp.gov.br>
2023-12-11 16:24:23 -0300

    Creating a Form for the Quiz

--- apress/php/livro_01/ch3/views/quiz.php
+++ apress/php/livro_01/ch3/views/quiz.php
@@ -1,3 +1,11 @@
 <?php
-$info= "quiz will go here";
+//complete code for views/quizform.php
+$info = "<form method='post' action='index.php?page=quiz'>
+<p>Is it hard fun to learn PHP?</p>
+<select name='answer'>
+<option value='yes'>Yes, it is</option>
+<option value='no'>No, not really</option>
+</select>
+<input type='submit' name='quiz-submitted' value='post' />
+</form>";
 ?>
