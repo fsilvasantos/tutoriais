91d1d7e76f7ef12e34ddc249f7bc59c0476b01dc
Felipe Santos <felipesantos@saovicente.sp.gov.br>
2023-12-11 14:57:22 -0300

    IMPROVEMENT - A Simple Search Form

--- apress/php/livro_01/ch3/views/search.php
+++ apress/php/livro_01/ch3/views/search.php
@@ -1,3 +1,6 @@
 <?php
-$info= "will soon show the search form";
+$info= "<form method='get' action='http://www.bing.com/search'>
+<input type='text' name='q' />
+<input type='submit' value='search on bing' />
+</form>";
 ?>
