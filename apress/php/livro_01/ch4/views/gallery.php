e4213a2ff7b53ccffdd38536638137b90bc66814
Felipe Santos <felipesantos@saovicente.sp.gov.br>
2023-12-15 15:07:15 -0300

    Secure Programming

--- apress/php/livro_01/ch4/views/gallery.php
+++ apress/php/livro_01/ch4/views/gallery.php
@@ -4,17 +4,50 @@
 function showImages() : string
 {
     $out = "<h1>Images Gallery</h1>";
-    $out .= "<ul id='images'>";
+    $out .= "<ul id='images'
+            style='
+            list-style-type:none;
+            width: 550px;
+            border: 5px solid black;
+            padding: 50px;
+            margin: 20px;'
+            ><li>";
+
+    $totalSize = 0;
+    $numberOfImages = 0;
+
     $dir_name = "imgs";
     chdir($dir_name);
+
     $images = glob("*.jpg");
 
     foreach($images as $image) 
     {
-        $out .= '<li><img src="'.$dir_name. '/' .$image.'" width="50%" /></li>';
+        //if((filesize($image) < 500000) and ($totalSize < 2500000)) -- limite muito estreito!!!
+        if((filesize($image) < 2000000) and ($totalSize < 5000000)) 
+        {
+            $out .= '<img src="'.$dir_name. '/' .$image.'"
+                    style="
+                    height: 200px;
+                    width: 250px;
+                    border: 2px solid black;
+                    padding: 5px;
+                    margin: 5px;
+                    "/>';
+
+            $totalSize += filesize($image);
+            $numberOfImages++;
+
+        }
+
+        if (($numberOfImages % 2) == 0) 
+        {
+            $out .= "</li><li>";
+        }
+
     }
 
-    $out .= "</ul>";
+    $out .= "</li></ul>";
 
     return $out;
 
