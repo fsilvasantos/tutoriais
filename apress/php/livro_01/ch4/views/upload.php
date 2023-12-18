4260f0f32b9102d1ef8d89e6a74aa4e7bf5f0a7b
Felipe Santos <felipesantos@saovicente.sp.gov.br>
2023-12-18 16:43:32 -0300

    Showing a Form for Uploading Images

--- apress/php/livro_01/ch4/views/upload.php
+++ apress/php/livro_01/ch4/views/upload.php
@@ -1,4 +1,9 @@
 <?php
-//complete source code for views/upload.php
-$info = "<h1>Upload New Images</h1>";
+$info = "
+<h1>Upload New jpg Images</h1>
+<form method='post' action='index.php?page=upload' enctype='multipart/form-data' >
+<label>Find a jpg image to upload</label>
+<input type='file' name='image-data' accept='image/jpeg'/>
+<input type='submit' value='upload' name='new-image' />
+</form>";
 ?>
